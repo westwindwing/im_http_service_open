212a9a0ffd37971df2fc51ed53ef483f9a90f678
binz.zhang <binz.zhang@qunar.com>
2019-02-22 10:39:34 +0800

    增量获取组织架构

---
+++ src/main/java/com/qunar/qchat/dao/IUserInfo.java
@@ -0,0 +1,40 @@
+package com.qunar.qchat.dao;
+
+
+import com.qunar.qchat.dao.model.UserInfoQtalk;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+/**
+ * @author binz.zhang
+ */
+@Component
+public interface IUserInfo {
+
+
+    /**
+     * select max version from the host_users
+     *
+     * @return
+     */
+    int selectMaxVersion(@Param("hostID") Integer host);
+
+    /**
+     * 获取公共域 host信息
+     *
+     * @param host
+     * @return
+     */
+    Integer getHostInfo(@Param("hostID") String host);
+
+    /**
+     * qtalk qunar 域获取员工信息
+     * @param version
+     * @return
+     */
+    List<UserInfoQtalk> getQtalkUsersByVersion(@Param("version") Integer version, @Param("hostID") Integer hostID);
+
+}
---
+++ src/main/java/com/qunar/qchat/dao/model/UserInfoQtalk.java
@@ -0,0 +1,78 @@
+package com.qunar.qchat.dao.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * UserInfoQtalk
+ *
+ * @author binz.zhang
+ * @date 2018/10/12
+ */
+@Getter
+@Setter
+public class UserInfoQtalk {
+
+    @JsonIgnore
+    private long host_id = 1;
+
+    @JsonProperty("U")
+    private String user_id;
+
+    @JsonIgnore
+    private String password = "";
+
+    @JsonProperty("N")
+    private String user_name;
+
+    @JsonProperty("D")
+    private String department = "";
+
+    @JsonIgnore
+    private String ps_deptid = "";
+
+    @JsonIgnore
+    private String dep1 = "";
+
+    @JsonIgnore
+    private String dep2 = "";
+
+    @JsonIgnore
+    private String dep3 = "";
+
+    @JsonIgnore
+    private String dep4 = "";
+
+    @JsonIgnore
+    private String dep5 = "";
+
+    @JsonProperty("pinyin")
+    private String pinyin = "";
+
+    @JsonIgnore
+    private String joinDate;
+
+    @JsonIgnore
+    private Integer hire_flag;
+
+    @JsonIgnore
+    private Integer version;
+
+    @JsonProperty("sex")
+    public Integer gender = 1;
+
+    @JsonIgnore
+    public String sex;
+
+    @JsonProperty("uType")
+    public String user_type = "";
+
+    @JsonProperty("email")
+    public String email = "";
+
+    @JsonIgnore
+    private int frozen_flag = 1;
+
+}
