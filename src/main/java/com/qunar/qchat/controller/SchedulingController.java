bcaf69687b51c9a1f0f9053a1a5476a5bba4bffd
memacs <boyofmonk@gmail.com>
2019-04-02 11:29:41 +0800

    修改代码

--- src/main/java/com/qunar/qchat/controller/SchedulingController.java
+++ src/main/java/com/qunar/qchat/controller/SchedulingController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
-@RequestMapping("/scheduling/")
+@RequestMapping("/newapi/scheduling/")
 @RestController
 public class SchedulingController {
     private static final Logger LOGGER = LoggerFactory.getLogger(SchedulingController.class);
